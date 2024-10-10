0881993a05f1450c65a5e5b2e20238ff3eb7f73a
tbilloud <tbilloud@gmail.com>
2024-10-10 14:05:23 +0200

    update python scripts

--- imaging/ComptonCamera_CCMod_paper/tools/plot_figure3.py
+++ imaging/ComptonCamera_CCMod_paper/tools/plot_figure3.py
@@ -6,12 +6,14 @@ import matplotlib.pyplot as plt
 import uproot
 
 path = '../output/CC_Singles.root'
+bins = 100
+
 pSingles = uproot.open(os.path.abspath(path) + ':Singles')
 print(pSingles.num_entries, 'singles')
 energy_scatterer = 1000 * pSingles.arrays(['energy'], '(energy>0) & (layerName=="scatterer_phys")')['energy']
 energy_absorber = 1000 *  pSingles.arrays(['energy'], '(energy>0) & (layerName=="absorber_phys")')['energy']
-fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(12, 6))
-plot_args = dict(bins=300, range=(0, 1500), histtype='step', color='blue')
+fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(16, 5))
+plot_args = dict(bins=bins, range=(0, 1500), histtype='step', color='blue')
 ax1.hist(np.array(energy_scatterer), **plot_args)
 ax1.set_title('Scatterer')
 ax1.set_xlabel('E1 (keV)', loc='right')
--- imaging/ComptonCamera_CCMod_paper/tools/plot_figure4.py
+++ imaging/ComptonCamera_CCMod_paper/tools/plot_figure4.py
@@ -17,40 +17,23 @@ path = '../output/CC_Coincidences.root'
 max_events = -1 # -1 for all
 
 events = uproot.open(os.path.abspath(path) + ':Coincidences')
-print(events.num_entries, 'events in tree Coincidences')
+print(events.num_entries, 'entries in tree Coincidences')
+bins = 100
 
 df = events.arrays(['coincID', 'energy', 'layerName'], '(energy>0)', library='pd', entry_stop=max_events)
 df_grouped = df.groupby('coincID')
 coincSize = df_grouped.size()
 for i in range(1, coincSize.max() + 1):
-    print('number of coincidences with', i, 'events:', len(coincSize[coincSize == i]), '(can be in same layer)' if i>1 else '(should be 0?)')
+    print('number of coincidences with', i, 'singles:', len(coincSize[coincSize == i]), '(can be in same layer, can be from different events)' if i>1 else '(should be 0?)')
 
-# CHOICE 1: Coincidences with 1 event in each layer
-# Coincidences with 2 events in different layers (slow because of filter function)
-# df = df.groupby('coincID').filter(lambda x: x['layerName'].nunique() == 2 and len(x) == 2)
-# print('number of coincidences with 2 events in different layers:', len(df))
-# e1 = np.array(df[df['layerName'] == 'absorber_phys']['energy'].values)
-# e2 = np.array(df[df['layerName'] == 'scatterer_phys']['energy'].values)
-# esum = e1 + e2
-
-# CHOICE 2: Coincidences disregarding number of events in layers
 df1 = events.arrays(['coincID', 'energy', 'layerName'], '(energy>0) & (layerName=="scatterer_phys")', library='pd', entry_stop=max_events)
 df2 = events.arrays(['coincID', 'energy', 'layerName'], '(energy>0) & (layerName=="absorber_phys")', library='pd', entry_stop=max_events)
-# SUB-CHOICE: don't sum energies in each layer
-# e1 = df1['energy']
-# e2 = df2['energy']
-# SUB-CHOICE: sum energies in each layer
-e1 = df1.groupby('coincID')['energy'].sum()
-e2 = df2.groupby('coincID')['energy'].sum()
-# Sum energies of both layers
-# ... TODO
-# df_sum = pandas.merge(df1, df2, on='coincID', how='inner')
-# print(df_sum)
-print(len(e1),len(e2))
+e1 = df1['energy']
+e2 = df2['energy']
 
 # Plot
 fig, (ax1, ax2, ax3) = plt.subplots(1, 3, figsize=(25, 5))
-plot_args = dict(bins=100, histtype='step', color='blue')
+plot_args = dict(bins=bins, histtype='step', color='blue')
 ax1.hist(1000 * e1, range=(0, 1400), **plot_args)
 ax1.set_title('first layer', y=-0.15)
 ax1.set_xlabel('E1 (keV)', loc='right')
