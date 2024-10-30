178c12d6bdfde969b0a16a241d66cdb255c3c95f
tbilloud <tbilloud@gmail.com>
2024-10-30 14:54:10 +0100

    update on scripts for figure 7

--- imaging/ComptonCamera_CCMod_paper/tools/plot_figure7_b.py
+++ imaging/ComptonCamera_CCMod_paper/tools/plot_figure7_b.py
@@ -1,3 +1,4 @@
+import sys
 import time
 import numpy as np
 import uproot
@@ -5,13 +6,17 @@ from uproot.extras import pandas
 import matplotlib.pyplot as plt
 import pandas as pd
 import napari
+pd.set_option('display.width', 1000), pd.set_option('display.max_columns', None)
 
 path = '../output/test_data/'
-energy_cut = '(energy1+energyR>0.6) & (energy1+energyR<1.275)'  # MeV
-E0 = 1.275  # MeV
-plane_side = 100  # mm, centered at (0, 0) in world coordinates
+energy_cut = '(energy1+energyR>0.6) & (energy1+energyR<1.275)'  # MeV, see section 2.2.3 of the paper
+E0 = 1.275  # MeV, incident gamma energy (adapt to energy_cut)
+plane_side = 100  # mm, plane being centered at (0, 0) in world coordinates
 plane_bins = 100
-plane_z = range(0, 51, 10)  # mm, z-coordinates of planes perpendicular
+plane_z = range(-50, 50, 1)  # mm, z-coordinates of planes perpendicular
+n_ellipse_points = 1000
+plane_cone_dirs = 1e-6
+
 xedges = np.linspace(-plane_side / 2, plane_side / 2, plane_bins + 1)
 yedges = np.linspace(-plane_side / 2, plane_side / 2, plane_bins + 1)
 # G4double m_E1; // energy deposition of the first interaction
@@ -23,72 +28,69 @@ yedges = np.linspace(-plane_side / 2, plane_side / 2, plane_bins + 1)
 
 tree = uproot.open(path + 'CC_Cones.root:Cones')
 print(tree.num_entries, 'entries in tree Cones')
-df_cone = tree.arrays(cut=energy_cut, library='pd')[2:4]
-tstart = time.time()
+df_cone = tree.arrays(cut=energy_cut, library='pd')[:]
+print('Number of cones with at least one NaN parameter:', df_cone.isnull().any(axis=1).sum())
+plane_normal = np.array([0, 0, 1])
 
 
-def stack_ellipses(row, histo, z_plane):
+def stack_ellipses(row, hist_stack, z_plane):
+
     # Extract necessary parameters from DataFrame
     apex = np.array([row['globalPosX1'], row['globalPosY1'], row['globalPosZ1']])
     direction = apex - [row['globalPosX2'], row['globalPosY2'], row['globalPosZ2']]
     direction = direction / np.linalg.norm(direction)
-    energy1 = row['energy1']
-    opening_angle = np.arccos(1 - (0.511 * energy1) / (E0 * (E0 - energy1)))
-    # check if opening angle is nan
-    if np.isnan(opening_angle):
+    E1 = row['energy1']
+    cosT = 1 - (0.511 * E1) / (E0 * (E0 - E1)) # equation 1a in paper
+
+    # Calculate ellipse parameters in plane
+    dot_product = np.dot(direction, plane_normal)
+    if np.abs(dot_product) < plane_cone_dirs:
+        print("The cone and plane are parallel and do not intersect.")
         return
-    else:
-        # Normalize the axis
-        plane_normal = np.array([0, 0, 1])
-        # Calculate the cosine and sine of the cone angle
-        cos_angle = np.cos(opening_angle)
-        sin_angle = np.sin(opening_angle)
-        # Calculate the dot product of the axis and the plane normal
-        dot_product = np.dot(direction, plane_normal)
-        # Check if the cone and plane are parallel
-        if np.abs(dot_product) < 1e-6:
-            print("The cone and plane are parallel and do not intersect.")
-            return
-        # Calculate the intersection point
-        d = (z_plane - apex[2]) / dot_product
-        intersect_p = apex + d * direction
-        # Calculate the radius of the intersection ellipse
-        radius = np.abs(d * sin_angle / cos_angle)  # Ensure radius is positive
-        # Calculate the direction of the major and minor axes of the ellipse
-        major_ax_d = np.cross(direction, plane_normal)
-        major_ax_d = major_ax_d / np.linalg.norm(major_ax_d)
-        minor_ax_d = np.cross(plane_normal, major_ax_d)
-        # Calculate the lengths of the major and minor axes
-        major_ax_l = radius / np.sqrt(1 - dot_product ** 2)
-        minor_ax_l = radius
-        theta = np.linspace(0, 2 * np.pi, 1000)
-        # Parametric equations for the ellipse
-        x_circle = intersect_p[0] + major_ax_l * np.cos(theta) * major_ax_d[0] + minor_ax_l * np.sin(theta) * minor_ax_d[0]
-        y_circle = intersect_p[1] + major_ax_l * np.cos(theta) * major_ax_d[1] + minor_ax_l * np.sin(theta) * minor_ax_d[1]
+    if 1 - cosT ** 2 <0:
+        print(row), print(cosT), sys.exit()
+    d = (z_plane - apex[2]) / dot_product
+    center = apex + d * direction # intersection point
+    sinT = np.sqrt(1 - cosT ** 2)
+    radius = np.abs(d * sinT / cosT)
+    maj_d = np.cross(direction, plane_normal) # direction of the major axis
+    maj_d = maj_d / np.linalg.norm(maj_d)
+    min_d = np.cross(plane_normal, maj_d) # direction of the minor axis
+    maj_l = radius / np.sqrt(1 - dot_product ** 2) # length of the major axis
+    min_l = radius # length of the minor axis
 
-        # Update the histogram
-        hist_update, _, _ = np.histogram2d(x_circle, y_circle, bins=[xedges, yedges])
-        ##############################################
-        # TODO: for 3D reconstruction, ellipse histograms should be weighted according to distance to apex
-        hist_update[hist_update > 0] = 1
-        hist_update *= radius**2
-        ##############################################
-        histo += hist_update
+    # Generate ellipse points
+    ellipse_points = np.linspace(0, 2 * np.pi,n_ellipse_points)
+    c = np.cos(ellipse_points)
+    s = np.sin(ellipse_points)
+    x = center[0] + maj_l * c * maj_d[0] + min_l * s * min_d[0]
+    y = center[1] + maj_l * c * maj_d[1] + min_l * s * min_d[1]
 
-hist_stack = np.zeros((len(plane_z), plane_bins, plane_bins))
+    # Update the histogram
+    hist_update, _, _ = np.histogram2d(x, y, bins=[xedges, yedges])
+    ##############################################
+    # TODO: for 3D reconstruction, ellipse histograms should be weighted according to distance to apex
+    hist_update[hist_update > 0] = 1
+    # hist_update *= abs(apex[2]-z_plane) # radius**2
+    ##############################################
+    hist_stack += hist_update
+
+vol = np.zeros((len(plane_z), plane_bins, plane_bins))
 for i, z in enumerate(plane_z):
     tstart = time.time()
-    hist, _, _ = np.histogram2d([], [], bins=[xedges, yedges])
-    df_cone.apply(lambda row: stack_ellipses(row, hist, z), axis=1)
-    print('Time taken:', time.time() - tstart)
+    hist_stack, _, _ = np.histogram2d([], [], bins=[xedges, yedges])
+    df_cone.apply(lambda row: stack_ellipses(row, hist_stack, z), axis=1)
+    print(i,'/',len(plane_z), 'time', round(time.time() - tstart,2),'s')
 
     # ### Multiple 2D histograms ###
-    plt.imshow(hist, origin='lower')
-    plt.colorbar()
-    plt.title(f'z = {z}')
-    plt.show()
+    # if i % 10 == 0:
+    #     plt.imshow(hist, origin='lower', vmax=(len(df_cone) * max(plane_z)/10))
+    #     plt.colorbar()
+    #     plt.title(f'z = {z}')
+    #     plt.show()
 
     ### 3D volume ###
-    hist_stack[i] = hist
-napari.view_image(hist_stack, rgb=False, colormap='viridis')
+    vol[i] = hist_stack
+
+napari.view_image(vol, rgb=False, colormap='viridis')
 napari.run()
--- imaging/ComptonCamera_CCMod_paper/tools/plot_figure7_b_fast.py
+++ imaging/ComptonCamera_CCMod_paper/tools/plot_figure7_b_fast.py
@@ -7,11 +7,14 @@ from uproot.extras import pandas
 import matplotlib.pyplot as plt
 
 path = '../output/test_data/'
-energy_cut = '(energy1+energyR>0.6) & (energy1+energyR<1.275)'  # MeV (adapt to E0)
+energy_cut = '(energy1+energyR>0.6) & (energy1+energyR<1.275)'  # MeV, see section 2.2.3 of the paper
 E0 = 1.275  # MeV, incident gamma energy (adapt to energy_cut)
-plane_side = 100  # mm, centered at (0, 0) in world coordinates
+plane_side = 100  # mm, plane being centered at (0, 0) in world coordinates
 plane_bins = 100
-plane_z = range(0, 1, 1)  # mm, z-coordinates of planes perpendicular
+plane_z = range(-50, 50, 1)  # mm, z-coordinates of planes perpendicular
+n_ellipse_points = 1000
+plane_cone_dirs = 1e-6
+
 xedges = cp.linspace(-plane_side / 2, plane_side / 2, plane_bins + 1)
 yedges = cp.linspace(-plane_side / 2, plane_side / 2, plane_bins + 1)
 # G4double m_E1; // energy deposition of the first interaction
@@ -23,58 +26,64 @@ yedges = cp.linspace(-plane_side / 2, plane_side / 2, plane_bins + 1)
 
 tree = uproot.open(path + 'CC_Cones.root:Cones')
 print(tree.num_entries, 'entries in tree Cones')
-df_cone = tree.arrays(cut=energy_cut, library='pd')
+df_cone = tree.arrays(cut=energy_cut, library='pd')[2:8]
+print('Number of cones with at least one NaN parameter:', df_cone.isnull().any(axis=1).sum())
+plane_normal = cp.array([0, 0, 1])
 
 
 def stack_ellipses(df, z_plane):
+
     # Extract necessary parameters from DataFrame
     apex = cp.array(df[['globalPosX1', 'globalPosY1', 'globalPosZ1']].values)
     direction = apex - cp.array(df[['globalPosX2', 'globalPosY2', 'globalPosZ2']].values)
     direction = direction / cp.linalg.norm(direction, axis=1, keepdims=True)
-    energy1 = cp.array(df['energy1'].values)
-    cosT = 1 - (0.511 * energy1) / (E0 * (E0 - energy1))
-    # TODO: check if opening angle is nan
+    E1 = cp.array(df['energy1'].values)
+    cosT = 1 - (0.511 * E1) / (E0 * (E0 - E1)) # equation 1a in paper
 
     # Calculate ellipse parameters in plane
     dot_products = direction[:, 2]
+    # if np.abs(dot_product) < plane_cone_dirs:
+    #     print("The cone and plane are parallel and do not intersect.")
+    #     return
     d = (z_plane - apex[:, 2]) / dot_products
     center = apex + d[:, None] * direction # intersection points between cone's axes and plane
     sinT = cp.sqrt(1 - cosT ** 2)
     radii = cp.abs(d * sinT / cosT)
-    maj_d = cp.cross(direction, cp.array([0, 0, 1])) # major axis direction
+    maj_d = cp.cross(direction, cp.array(plane_normal)) # major axis direction
     maj_d /= cp.linalg.norm(maj_d, axis=1, keepdims=True) # normalize major axis direction
-    min_d = cp.cross(cp.array([0, 0, 1]), maj_d) # minor axis direction (normalized)
+    min_d = cp.cross(cp.array(plane_normal), maj_d) # minor axis direction (normalized)
     maj_l = radii / cp.sqrt(1 - dot_products ** 2) # major axis length
     min_l = radii # minor axis length
 
     # Generate ellipse points
-    ellipse_angle_grid = cp.linspace(0, 2 * cp.pi, 1000)
-    c = cp.cos(ellipse_angle_grid)
-    s = cp.sin(ellipse_angle_grid)
+    ellipse_points = cp.linspace(0, 2 * cp.pi, n_ellipse_points)
+    c = cp.cos(ellipse_points)
+    s = cp.sin(ellipse_points)
     x = center[:, 0][:, None] + maj_l[:, None] * c * maj_d[:, 0][:, None] + min_l[:, None] * s * min_d[:, 0][:, None]
     y = center[:, 1][:, None] + maj_l[:, None] * c * maj_d[:, 1][:, None] + min_l[:, None] * s * min_d[:, 1][:, None]
 
-    hist, _, _ = cp.histogram2d(x.ravel(), y.ravel(), bins=[xedges, yedges], density=False)
+    # Fill the histogram
+    hist_stack, _, _ = cp.histogram2d(x.ravel(), y.ravel(), bins=[xedges, yedges], density=True)
     ##############################################
     # TODO: for 3D reconstruction, ellipse histograms should be weighted according to distance to apex
-    # hist[hist > 0] = 1 # TODO binarize individual ellipses instead of the whole stack
+    # hist_stack[hist_stack > 0] = 1 # TODO binarize individual ellipses instead of the whole stack
     ##############################################
-    return hist
+    return hist_stack
 
 
 # ### Multiple 2D histograms ###
-for z in plane_z:
-    tstart = time.time()
-    hist = stack_ellipses(df_cone, z)
-    print('Time taken:', time.time() - tstart)
-    plt.imshow(hist.get(), origin='lower')
-    plt.colorbar()
-    plt.title(f'z = {z}')
-    plt.show()
+# for z in plane_z:
+#     tstart = time.time()
+#     hist = stack_ellipses(df_cone, z)
+#     print('Time taken:', time.time() - tstart)
+#     plt.imshow(hist.get(), origin='lower')
+#     plt.colorbar()
+#     plt.title(f'z = {z}')
+#     plt.show()
 
 # ### 3D volume ####
-# tstart = time.time()
-# hist_stack = cp.array([stack_ellipses(df_cone[:], z) for z in plane_z])
-# print('Time taken:', time.time() - tstart)
-# napari.view_image(hist_stack.get(), rgb=False, colormap='viridis')
-# napari.run()
+tstart = time.time()
+vol = cp.array([stack_ellipses(df_cone[:], z) for z in plane_z])
+print('Time taken:', time.time() - tstart)
+napari.view_image(vol.get(), rgb=False, colormap='viridis')
+napari.run()
