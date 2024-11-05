fa7e359b49a93ca9053ca191ae16b6f3f8f89316
tbilloud <tbilloud@gmail.com>
2024-11-05 16:15:57 +0100

    improve print scripts

--- imaging/ComptonCamera_CCMod_paper/tools/print_hits.py
+++ imaging/ComptonCamera_CCMod_paper/tools/print_hits.py
@@ -1,24 +1,27 @@
-import os
-from operator import index
 import pandas
-import awkward as ak
 import uproot
 import numpy as np
+
 pandas.set_option('display.max_columns', 100), pandas.set_option('display.width', 400), pandas.set_option(
     'display.max_rows', 1000)
 
-print(uproot.open('../output/temp/CC_Hits.root').keys())
-tree_hits = uproot.open('../output/CC_Hits.root:Hits')
-print(tree_hits.num_entries, 'entries in tree Hits')
-print(np.unique(tree_hits["eventID"].array(library="np")).size, 'unique eventIDs')
-print(np.unique(tree_hits["trackID"].array(library="np")).size, 'unique trackIDs')
-print(np.unique(tree_hits["parentID"].array(library="np")).size, 'unique parentIDs')
+# path = '../output/test_data'
+path = '/media/billoud/Volume/CT/GATE/seed1_time100/'
 
+# Print info about hits
+tree_hits = uproot.open(path + 'CC_Hits.root:Hits')
+print(tree_hits.num_entries, 'entries in tree Hits')
 df = tree_hits.arrays(
-    ["eventID", "PDGEncoding", "trackID", "parentID", "time", "edep", "stepLength", "sourceEnergy",
-     "energyFinal", "energyIniT", "postStepProcess", "processName", "layerName"],
-    #'eventID == 323',
-    library='pd')
-print(df[:100])
-# print(df['eventID'].unique()) # 153  250  299  323  333  388  464  586  604  624  672
-print(df['sourceEnergy'].unique())
+    # ["eventID", "PDGEncoding", "trackID", "parentID", "time", "edep", "stepLength", "sourceEnergy"],
+    # 'eventID == 323',
+    library='pd',
+    entry_start=0,
+    entry_stop=10, # -1 for last entry
+)
+print(df[:])
+
+# Extra info
+# print(np.unique(tree_hits["eventID"].array(library="np")).size, 'unique eventIDs')
+# print(np.unique(tree_hits["trackID"].array(library="np")).size, 'unique trackIDs')
+# print(np.unique(tree_hits["parentID"].array(library="np")).size, 'unique parentIDs')
+# print(df['sourceEnergy'].unique())
---
+++ imaging/ComptonCamera_CCMod_paper/tools/print_sequence_coincidence.py
@@ -0,0 +1,15 @@
+
+import pandas
+import uproot
+pandas.set_option('display.max_columns', 100), pandas.set_option('display.width', 400), pandas.set_option(
+    'display.max_rows', 1000)
+
+path = '../output/test_data/'
+nevent_printed = 10
+
+# Print info about coincidence sequences
+tree = uproot.open(path+'CC_sequenceCoincidence.root')
+print(tree.keys())
+# print(tree.num_entries, 'entries in tree sequenceCoincidence')
+# df_SC = tree.arrays(library='pd')
+# print(df_SC[:5])
