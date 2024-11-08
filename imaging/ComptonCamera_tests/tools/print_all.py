43d6dbf69b2f539e97d71e78bf652bd75026c73b
tbilloud <tbilloud@gmail.com>
2024-11-08 12:18:32 +0100

    clean up code

--- imaging/ComptonCamera_tests/tools/print_all.py
+++ imaging/ComptonCamera_tests/tools/print_all.py
@@ -1,29 +1,33 @@
-import os
-from operator import index
 import pandas
-import awkward as ak
 import uproot
-import numpy as np
-from matplotlib.style.core import library
-pandas.set_option('display.max_columns', 100), pandas.set_option('display.width', 400), pandas.set_option(
-    'display.max_rows', 1000)
 
-path = '../output/test_data/'
-#path = '/media/billoud/Volume/CT/GATE/LaBr3_adder_thl_100/'
+pandas.set_option('display.max_columns', 100)
+pandas.set_option('display.width', 400)
+pandas.set_option('display.max_rows', 1000)
+
+path = '../output/'
 nevent_printed = 10
 
+# Not processing Hits tree because I don't always save it (very large file)
+
 # Print some info about 'singles' first
-tree = uproot.open(path+'CC_Singles.root:Singles')
+tree = uproot.open(path + 'CC_Singles_new.root:Singles')
 print(tree.num_entries, 'entries in tree Singles')
-df = tree.arrays(["eventID", "layerName", "time", "round(1000*energy)"],library='pd')
+df = tree.arrays(["eventID", "layerName", "time", "round(1000*energy)"], library='pd')
 
 # Print info about coincidences
-tree = uproot.open(path+'CC_Coincidences.root:Coincidences')
+tree = uproot.open(path + 'CC_Coincidences.root:Coincidences')
 print(tree.num_entries, 'entries in tree Coincidences')
-df = tree.arrays(["eventID ", 'coincID', "layerName", "time", "round(1000*energy)"],library='pd')
+df = tree.arrays(["eventID ", 'coincID', "layerName", "time", "round(1000*energy)"], library='pd')
 
 # Print info about coincidence sequences
-tree = uproot.open(path+'CC_sequenceCoincidence.root:sequenceCoincidence')
+tree = uproot.open(path + 'CC_sequenceCoincidence.root:sequenceCoincidence')
 print(tree.num_entries, 'entries in tree sequenceCoincidence')
-df_SC = tree.arrays(library='pd')
-print(df_SC[:5])
+df = tree.arrays(library='pd')
+print(df[:5])
+
+# Print info about cones
+tree = uproot.open(path + 'CC_Cones.root:Cones')
+print(tree.num_entries, 'entries in tree sequenceCoincidence')
+df = tree.arrays(library='pd')
+print(df[:5])
