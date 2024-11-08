05048736b84ffd30cec3ad17e2a5c01515808044
tbilloud <tbilloud@gmail.com>
2024-11-08 09:49:56 +0100

    clean up code

--- imaging/ComptonCamera_CCMod_paper/tools/print_sequence_coincidence.py
+++ imaging/ComptonCamera_CCMod_paper/tools/print_sequence_coincidence.py
@@ -8,8 +8,7 @@ path = '../output/test_data/'
 nevent_printed = 10
 
 # Print info about coincidence sequences
-tree = uproot.open(path+'CC_sequenceCoincidence.root')
-print(tree.keys())
-# print(tree.num_entries, 'entries in tree sequenceCoincidence')
-# df_SC = tree.arrays(library='pd')
-# print(df_SC[:5])
+tree = uproot.open(path+'CC_sequenceCoincidence.root:sequenceCoincidence')
+print(tree.num_entries, 'entries in tree sequenceCoincidence')
+df_SC = tree.arrays(library='pd')
+print(df_SC[:5])
