c4d6198880f7bd065f27a70404616c3e6ed80e3d
tbilloud <tbilloud@gmail.com>
2024-11-11 16:05:53 +0100

    add some info

--- imaging/ComptonCamera_tests/tools/print_all.py
+++ imaging/ComptonCamera_tests/tools/print_all.py
@@ -5,35 +5,45 @@ pandas.set_option('display.max_columns', 100)
 pandas.set_option('display.width', 400)
 pandas.set_option('display.max_rows', 1000)
 
+# Script to print some info about root trees produced by CCMod actor
+# ! WARNING! comment out blocks if corresponding tree was not saved
 path = '../output/'
-nevent_printed = 10
+nentries_printed = 3
 
-# Print some info about 'hist' => comment out if hit tree was not saved
-tree = uproot.open(path + 'CC_Hits.root:Hits')
-print(tree.num_entries, 'entries in tree Hits')
-df = tree.arrays(library='pd')
-print(df[df['eventID'] == 22819])
+# Info about electron escape
+# tree = uproot.open(path + 'CC_eventGlobalInfo.root:EventGlobalInfo')
+# print(tree.num_entries, 'entries in tree Hits')
+# df = tree.arrays(library='pd')
+# print(df[:nevent_printed])
 
-# Print some info about 'singles'
+# Info about 'hits'
+# tree = uproot.open(path + 'CC_Hits.root:Hits')
+# print(tree.num_entries, 'entries in tree Hits')
+# df = tree.arrays(library='pd')
+# print(df[df['eventID'] == 7043])
+# # print(df[:nevent_printed])
+
+# Info about 'singles'
 tree = uproot.open(path + 'CC_Singles.root:Singles')
 print(tree.num_entries, 'entries in tree Singles')
 df = tree.arrays(library='pd')
-print(df[:5])
+# print(df[df['eventID'] == 25745])
+# print(df[:nevent_printed])
 
-# Print info about coincidences
+# PrintInfo about 'coincidences'
 tree = uproot.open(path + 'CC_Coincidences.root:Coincidences')
 print(tree.num_entries, 'entries in tree Coincidences')
 df = tree.arrays(library='pd')
-print(df[:5])
+print(df[:nentries_printed])
 
-# Print info about coincidence sequences
+# PrintInfo about 'coincidence sequences'
 tree = uproot.open(path + 'CC_sequenceCoincidence.root:sequenceCoincidence')
 print(tree.num_entries, 'entries in tree sequenceCoincidence')
 df = tree.arrays(library='pd')
-print(df[:5])
+print(df[:nentries_printed])
 
-# Print info about cones => comment out if cone tree was not created (has to be done after simulation)
+# # PrintInfo about 'cones' => comment out if cone tree was not created (has to be done after simulation)
 tree = uproot.open(path + 'CC_Cones.root:Cones')
 print(tree.num_entries, 'entries in tree Cones')
 df = tree.arrays(library='pd')
-print(df[:5])
+print(df[:])
