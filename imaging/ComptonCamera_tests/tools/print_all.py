58b85aa3fc4d80f17fd5b22a0a1a8d2c1fc69b08
tbilloud <tbilloud@gmail.com>
2024-11-13 13:38:17 +0100

    add source volume for rotation

--- imaging/ComptonCamera_tests/tools/print_all.py
+++ imaging/ComptonCamera_tests/tools/print_all.py
@@ -7,7 +7,7 @@ pandas.set_option('display.max_rows', 1000)
 
 # Script to print some info about root trees produced by CCMod actor
 # ! WARNING! comment out blocks if corresponding tree was not saved
-path = '../single_layer/output/'
+path = '../vox_source/output/'
 nentries_printed = 3
 
 # Info about electron escape
@@ -42,13 +42,13 @@ print(tree.num_entries, 'entries in tree sequenceCoincidence')
 df = tree.arrays(library='pd')
 print(df[:nentries_printed])
 
-# PrintInfo about 'cones' => comment out if cone tree was not created (has to be done after simulation)
-tree = uproot.open(path + 'CC_Cones.root:Cones')
-print(tree.num_entries, 'entries in tree Cones')
-df = tree.arrays(library='pd')
-print(df[:nentries_printed])
-print(len(df[df['nSingles'] == 1]),'entries with nSingles == 1')
-print(len(df[df['nSingles'] == 2]),'entries with nSingles == 2')
-print(len(df[df['nSingles'] == 3]),'entries with nSingles == 3')
-print(len(df[df['nSingles'] == 4]),'entries with nSingles == 4')
-print(len(df[df['nSingles'] == 5]),'entries with nSingles == 5')
+# # PrintInfo about 'cones' => comment out if cone tree was not created (has to be done after simulation)
+# tree = uproot.open(path + 'CC_Cones.root:Cones')
+# print(tree.num_entries, 'entries in tree Cones')
+# df = tree.arrays(library='pd')
+# print(df[:nentries_printed])
+# print(len(df[df['nSingles'] == 1]),'entries with nSingles == 1')
+# print(len(df[df['nSingles'] == 2]),'entries with nSingles == 2')
+# print(len(df[df['nSingles'] == 3]),'entries with nSingles == 3')
+# print(len(df[df['nSingles'] == 4]),'entries with nSingles == 4')
+# print(len(df[df['nSingles'] == 5]),'entries with nSingles == 5')
