134d2076acfbe8d4575eb17fbe3e9a4577841c2c
tbilloud <tbilloud@gmail.com>
2024-11-13 13:16:07 +0100

    add reco scripts

---
+++ imaging/ComptonCamera_tests/tools/reco_multiCamera.py
@@ -0,0 +1,36 @@
+import os
+import cupy as cp
+import napari
+from compton import compton_forward
+from seqCoinc2ConesArray_ideal import seqCoin2Cones
+
+# Same as reco.py but combining multiple root files
+
+##############################################################
+# Settings
+##############################################################
+path = '../point_source/output/'
+E0 = 0.250  # MeV, incident gamma energy (adapt to energy_cut)
+vsize = (256, 256, 256)
+vpitch = 1
+er = 100  # inverse of cosine error
+nSingles_max = 2  # maximum number of singles per coincidence, set to False to disable
+true_coinc = False  # filter true coincidences (i.e. avoid singles from different events)
+
+##############################################################
+# Do Projection
+##############################################################
+fnames = [path + sd + '/CC_sequenceCoincidence.root' for sd in os.listdir(path)]
+cp_array = cp.concatenate([seqCoin2Cones(fn, E0, vsize, vpitch, er, nSingles_max, true_coinc) for fn in fnames], axis=0)
+vol = cp.zeros(vsize, dtype=cp.float32)
+vol = compton_forward(vol, cp_array, volume_pitch=vpitch)
+
+##############################################################
+# Display/Save results
+##############################################################
+vol = vol.get()
+# cp.save("vol.npy", vol)
+viewer = napari.view_image(vol, translate=(-vsize[0] // 2, -vsize[1] // 2, -vsize[2] // 2), colormap='gray_r',
+                           axis_labels=["y", "x", "z"])
+viewer.axes.visible = True
+napari.run()
