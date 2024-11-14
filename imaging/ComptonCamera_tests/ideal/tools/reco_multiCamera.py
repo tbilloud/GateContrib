ac99c0e8292d32230dc8b68fa99e8516123abc3a
tbilloud <tbilloud@gmail.com>
2024-11-14 15:30:54 +0100

    adapt to new format

--- imaging/ComptonCamera_tests/ideal/tools/reco_multiCamera.py
+++ imaging/ComptonCamera_tests/ideal/tools/reco_multiCamera.py
@@ -9,10 +9,9 @@ from pathlib import Path
 ##############################################################
 # Settings
 ##############################################################
-path = Path('../sourcePoint_cameraDouble/output/')
-E0 = 0.250  # MeV, incident gamma energy (adapt to energy_cut)
+path, E0_MeV, world_mm = Path('../sourceRectangles_cameraSingle/output/'), 0.250, 20
 vsize = (256, 256, 256)
-vpitch = 1
+vpitch = world_mm / vsize[2]
 er = 100  # inverse of cosine error
 nSingles_max = 2  # maximum number of singles per coincidence, set to False to disable
 true_coinc = False  # filter true coincidences (i.e. avoid singles from different events)
@@ -22,7 +21,7 @@ nentries = None
 # Do Projection
 ##############################################################
 fnames = [sd / 'CC_sequenceCoincidence.root' for sd in path.iterdir() if sd.is_dir()]
-cp_array = cp.concatenate([seqCoin2ConesArray(fn, E0, vsize, vpitch, er, nSingles_max, true_coinc, nentries) for fn in fnames], axis=0)
+cp_array = cp.concatenate([seqCoin2ConesArray(fn, E0_MeV, vsize, vpitch, er, nSingles_max, true_coinc, nentries) for fn in fnames], axis=0)
 vol = cp.zeros(vsize, dtype=cp.float32)
 vol = compton_forward(vol, cp_array, volume_pitch=vpitch)
 
@@ -31,7 +30,7 @@ vol = compton_forward(vol, cp_array, volume_pitch=vpitch)
 ##############################################################
 vol = vol.get()
 cp.save(fnames[0].parent / "reconstruction.npy", vol)
-viewer = napari.view_image(vol, translate=(-vsize[0] // 2, -vsize[1] // 2, -vsize[2] // 2), colormap='gray_r',
-                           axis_labels=["y", "x", "z"])
+vargs = dict(translate=(-vsize[0] // 2, -vsize[1] // 2, -vsize[2] // 2), colormap='gray_r', axis_labels=["y", "x", "z"])
+viewer = napari.view_image(vol, **vargs)
 viewer.axes.visible = True
 napari.run()
