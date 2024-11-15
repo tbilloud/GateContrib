6285d0d1fafade26f01981941ac2420bf7ca4834
tbilloud <tbilloud@gmail.com>
2024-11-15 13:50:11 +0100

    update

--- imaging/ComptonCamera_tests/tools/print_all.py
+++ imaging/ComptonCamera_tests/tools/print_all.py
@@ -7,7 +7,7 @@ pandas.set_option('display.max_rows', 1000)
 
 # Script to print some info about root trees produced by CCMod actor
 # ! WARNING! comment out blocks if corresponding tree was not saved
-path = '../ideal/sourceVox_cameraSingle/output/'
+path = '../ideal/sourceRectangles_cameraSingle/output/time1000_cameraX5Y0Z9/'
 nentries_printed = 3
 
 # Info about electron escape
@@ -26,29 +26,30 @@ nentries_printed = 3
 # Info about 'singles'
 tree = uproot.open(path + 'CC_Singles.root:Singles')
 print(tree.num_entries, 'entries in tree Singles')
-df = tree.arrays(library='pd')
+df = tree.arrays(library='pd', entry_stop=nentries_printed)
 # print(df[df['eventID'] == 25745])
 # print(df[:nevent_printed])
 
 # PrintInfo about 'coincidences'
 tree = uproot.open(path + 'CC_Coincidences.root:Coincidences')
 print(tree.num_entries, 'entries in tree Coincidences')
-df = tree.arrays(library='pd')
-print(df[:nentries_printed])
+df = tree.arrays(library='pd', entry_stop=nentries_printed)
+print(df)
 
 # PrintInfo about 'coincidence sequences'
 tree = uproot.open(path + 'CC_sequenceCoincidence.root:sequenceCoincidence')
 print(tree.num_entries, 'entries in tree sequenceCoincidence')
-df = tree.arrays(library='pd')
-print(df[:nentries_printed])
+df = tree.arrays(library='pd', entry_stop=nentries_printed)
+print(df)
 
-# # PrintInfo about 'cones' => comment out if cone tree was not created (has to be done after simulation)
-# tree = uproot.open(path + 'CC_Cones.root:Cones')
-# print(tree.num_entries, 'entries in tree Cones')
-# df = tree.arrays(library='pd')
-# print(df[:nentries_printed])
-# print(len(df[df['nSingles'] == 1]),'entries with nSingles == 1')
-# print(len(df[df['nSingles'] == 2]),'entries with nSingles == 2')
-# print(len(df[df['nSingles'] == 3]),'entries with nSingles == 3')
-# print(len(df[df['nSingles'] == 4]),'entries with nSingles == 4')
-# print(len(df[df['nSingles'] == 5]),'entries with nSingles == 5')
+# PrintInfo about 'cones' => comment out if cone tree was not created (has to be done after simulation)
+tree = uproot.open(path + 'CC_Cones.root:Cones')
+print(tree.num_entries, 'entries in tree Cones')
+df = tree.arrays(library='pd', entry_stop=nentries_printed)
+print(df)
+# Set entry_stop to -1 if using the following
+print(len(df[df['nSingles'] == 1]),'entries with nSingles == 1')
+print(len(df[df['nSingles'] == 2]),'entries with nSingles == 2')
+print(len(df[df['nSingles'] == 3]),'entries with nSingles == 3')
+print(len(df[df['nSingles'] == 4]),'entries with nSingles == 4')
+print(len(df[df['nSingles'] == 5]),'entries with nSingles == 5')
