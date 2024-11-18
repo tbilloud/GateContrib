f291ce392efcc840c84a8d5171ba4889c4690995
tbilloud <tbilloud@gmail.com>
2024-11-18 14:42:17 +0100

    new simu

--- imaging/ComptonCamera_tests/ideal/tools/reco.py
+++ imaging/ComptonCamera_tests/ideal/tools/reco.py
@@ -1,7 +1,7 @@
 import cupy as cp
 import napari
 from pathlib import Path
-from imaging.ComptonCamera_tests.ideal.tools.seqCoinc2Cones import seqCoin2ConesArray, conesTTree2conesArray
+from imaging.ComptonCamera_tests.ideal.tools.seqCoinc2Cones import *
 from imaging.ComptonCamera_tests.tools.compton import compton_forward
 
 # Script to reconstruct the source from root files with a sequenceCoincidence tree
@@ -12,21 +12,21 @@ from imaging.ComptonCamera_tests.tools.compton import compton_forward
 # fname, E0, world_z_mm = Path('../sourceRectangle_cameraSingle/output/1MBq_time1000'), 0.250, 200
 # fname, E0_MeV, world_mm = Path('../sourceRectangles_cameraSingle/output'), 0.250, 20
 # fname, E0_MeV, world_mm = Path('../sourceRectangles_cameraSingle/output/time100_cameraX5Y0Z9'), 0.250, 20
-fname, E0_MeV, world_mm = Path('/media/billoud/Volume/CT/GATE/ideal/time1_camera_posX0_posY0'), 0.250, 40
+fname, E0_MeV, world_mm = Path('/media/billoud/Volume/CT/GATE/ideal/sourceSurfaces/time2000_camera_posX10_posY10'), 0.250, 40
 vsize = (256, 256, 256)
 vpitch = world_mm / vsize[2]
 er = 100  # inverse of cosine error
 nSingles_max = 2  # maximum number of singles per coincidence, set to False to disable
 true_coinc = True  # filter true coincidences (i.e. avoid singles from different events)
-nentries = None
+nentries = 100000 # None to read all entries
 
 ##############################################################
 # Do Projection
 ##############################################################
 # ###### READING sequenceCoincidence.root files ##############
-array = seqCoin2ConesArray(fname / 'CC_sequenceCoincidence.root', E0_MeV, vsize, vpitch, er, nSingles_max, true_coinc, nentries)
+# array = seqCoin2ConesArray(fname / 'CC_sequenceCoincidence.root', E0_MeV, vsize, vpitch, er, nSingles_max, true_coinc, nentries)
 # ###### READING CC_Cones.root files #########################
-# array = conesTTree2conesArray(fname / 'CC_Cones.root', E0_MeV, vsize, vpitch, er, nSingles_max, true_coinc, nentries)
+array = conesTTree2conesArray(fname / 'CC_Cones.root', E0_MeV, vsize, vpitch, er, nSingles_max, true_coinc, nentries)
 
 vol = cp.zeros(vsize, dtype=cp.float32)
 vol = compton_forward(vol, array, volume_pitch=vpitch)
