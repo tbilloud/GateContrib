a8ff9009486c6cbdbe435555944489db1557bfc4
tbilloud <tbilloud@gmail.com>
2024-11-19 15:36:11 +0100

    new simu

--- imaging/ComptonCamera_tests/ideal/tools/save_multi_coneTTree.py
+++ imaging/ComptonCamera_tests/ideal/tools/save_multi_coneTTree.py
@@ -3,7 +3,7 @@ from pathlib import Path
 
 # When reconstructing large files (> 1M cones), better to save cone TTrees rather than reading sequenceCoincidence.root
 
-path = Path('../sourceRectangles_cameraSingle/output/')
+path = Path('/media/billoud/Volume/CT/GATE/ideal/sourceCube/140keV')
 fnames = list(path.rglob('CC_sequenceCoincidence.root'))
 for fn in fnames:
     seqCoinc2ConesTTree(fn, fn.parent / 'CC_Cones.root')
