a5de6661477b980b15414ef04bac9fb95973a24f
tbilloud <tbilloud@gmail.com>
2024-11-21 13:59:24 +0100

    new tests

--- imaging/ComptonCamera_tests/ideal/tools/reco.py
+++ imaging/ComptonCamera_tests/ideal/tools/reco.py
@@ -18,22 +18,23 @@ from imaging.ComptonCamera_tests.tools.utils import remove_nans
 # fname, E0_MeV, world_mm = Path('/media/billoud/Volume/CT/GATE/ideal/sourceSurfaces/time2000_camera_posX10_posY10'), 0.250, 40
 # fname, E0_MeV, world_mm = Path('/media/billoud/Volume/CT/GATE/ideal/sourceVolumes/140keV/time200_camera_posX0_posY0'), 0.1405, 40
 # fname, E0_MeV, world_mm = Path('/media/billoud/Volume/CT/GATE/ideal/sourceVolumes/245keV/time200_camera_posX0_posY0'), 0.245, 40
-fname, E0_MeV, world_mm = Path('/media/billoud/Volume/CT/GATE/ideal/sourceCube/140keV/time1000/time1000_seed1_cameraX0Y0Z10'), 0.140, 40
+# fname, E0_MeV, world_mm = Path('/media/billoud/Volume/CT/GATE/ideal/sourceCube/140keV/time1000/time1000_seed1_cameraX0Y0Z10'), 0.140, 40
+fname, E0_MeV, world_mm = Path('../sourcePoint_cameraSingle/output/livermore'), 0.100, 200
 
 vsize = (256, 256, 256)
 vpitch = world_mm / vsize[2]
 er = 100  # inverse of cosine error
 nSingles_max = 2  # maximum number of singles per coincidence, set to False to disable
 true_coinc = True  # filter true coincidences (i.e. avoid singles from different events)
-nentries = None  # None to read all entries
+nentries = 1000  # None to read all entries
 
 ##############################################################
 # Do Projection
 ##############################################################
 # ###### READING sequenceCoincidence.root files ##############
-# cones = seqCoin2ConesArray(fname / 'CC_sequenceCoincidence.root', E0_MeV, vsize, vpitch, er, nSingles_max, true_coinc, nentries)
+cones = seqCoin2ConesArray(fname / 'CC_sequenceCoincidence.root', E0_MeV, vsize, vpitch, er, nSingles_max, true_coinc, nentries)
 # ###### READING CC_Cones.root files #########################
-cones = conesTTree2conesArray(fname / 'CC_Cones.root', E0_MeV, vsize, vpitch, er, nSingles_max, true_coinc, nentries)
+# cones = conesTTree2conesArray(fname / 'CC_Cones.root', E0_MeV, vsize, vpitch, er, nSingles_max, true_coinc, nentries)
 # ######## RECONSTRUCT #######################################
 cones = remove_nans(cones)
 vol = cp.zeros(vsize, dtype=cp.float32)
