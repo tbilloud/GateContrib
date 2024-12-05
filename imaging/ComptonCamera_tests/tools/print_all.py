23cde0fe467dc525af3d7594ba8f3cc8c1fb803e
tbilloud <tbilloud@gmail.com>
2024-12-05 17:14:48 +0100

    reorganize script

--- imaging/ComptonCamera_tests/tools/print_all.py
+++ imaging/ComptonCamera_tests/tools/print_all.py
@@ -9,7 +9,7 @@ pandas.set_option('display.max_rows', 1000)
 # ! WARNING! comment out blocks if corresponding tree was not saved
 path = '/home/billoud/PycharmProjects/GateContrib/imaging/ComptonCamera_tests/ideal/sourcePoint_cameraSingle/output/'
 #path = '/media/billoud/Volume/CT/GATE/ideal/time1_camera_posX0_posY0/'
-nentries_printed = 10 # None to read all entries
+nentries_printed = None # None to read all entries
 
 # Info about electron escape
 tree = uproot.open(path + 'CC_eventGlobalInfo.root:EventGlobalInfo')
@@ -20,39 +20,42 @@ print(tree.num_entries, 'entries in tree EventGlobalInfo')
 # Info about 'hits'
 tree = uproot.open(path + 'CC_Hits.root:Hits')
 print(tree.num_entries, 'entries in tree Hits')
-df = tree.arrays(library='pd', entry_stop=nentries_printed)
-#print(df[df['eventID'] == 6])
-#print(df)
+hits = tree.arrays(library='pd', entry_stop=nentries_printed)
+# print(hits)
+# print(hits[hits['postStepProcess'] == 'Rayl'])
 
 # Info about 'singles'
 tree = uproot.open(path + 'CC_Singles.root:Singles')
 print(tree.num_entries, 'entries in tree Singles')
-df = tree.arrays(library='pd', entry_stop=nentries_printed)
-# print(df[df['eventID'] == 6])
+singles = tree.arrays(library='pd', entry_stop=nentries_printed)
 # print(df)
 
 # PrintInfo about 'coincidences'
 tree = uproot.open(path + 'CC_Coincidences.root:Coincidences')
 print(tree.num_entries, 'entries in tree Coincidences')
-df = tree.arrays(library='pd', entry_stop=nentries_printed)
-# print(df[df['eventID'] == 6])
-# print(df)
+coincidences = tree.arrays(library='pd', entry_stop=nentries_printed)
+# print(coincidences)
 
 # PrintInfo about 'coincidence sequences'
 tree = uproot.open(path + 'CC_sequenceCoincidence.root:sequenceCoincidence')
 print(tree.num_entries, 'entries in tree sequenceCoincidence')
-df = tree.arrays(library='pd', entry_stop=nentries_printed)
-#print(df[df['eventID'] == 6])
-print(df)
+seqCoin = tree.arrays(library='pd', entry_stop=nentries_printed)
+#print(seqCoin[:50])
+print(seqCoin[seqCoin['coincID'] == 48])
 
 # # PrintInfo about 'cones' => comment out if cone tree was not created (has to be done after simulation)
 # tree = uproot.open(path + 'CC_Cones.root:Cones')
 # print(tree.num_entries, 'entries in tree Cones')
-# df = tree.arrays(library='pd', entry_stop=nentries_printed)
-# print(df)
+# cones = tree.arrays(library='pd', entry_stop=nentries_printed)
+# print(cones)
 # # Set entry_stop to -1 if using the following
-# print(len(df[df['nSingles'] == 1]),'entries with nSingles == 1')
-# print(len(df[df['nSingles'] == 2]),'entries with nSingles == 2')
-# print(len(df[df['nSingles'] == 3]),'entries with nSingles == 3')
-# print(len(df[df['nSingles'] == 4]),'entries with nSingles == 4')
-# print(len(df[df['nSingles'] == 5]),'entries with nSingles == 5')
+# print(len(cones[cones['nSingles'] == 1]),'entries with nSingles == 1')
+# print(len(cones[cones['nSingles'] == 2]),'entries with nSingles == 2')
+# print(len(cones[cones['nSingles'] == 3]),'entries with nSingles == 3')
+
+# To use with nentries_printed = None
+eventId = 96212
+print(hits[hits['eventID'] == eventId])
+print(singles[singles['eventID'] == eventId])
+print(coincidences[coincidences['eventID'] == eventId])
+print(seqCoin[seqCoin['eventID'] == eventId])
