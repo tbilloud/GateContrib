cb1b23057380e73d6319f4189f0c9e151c321c98
tbilloud <tbilloud@gmail.com>
2024-12-09 17:26:44 +0100

    investigating low coincidence energies

--- imaging/ComptonCamera_tests/tools/plot_spectra.py
+++ imaging/ComptonCamera_tests/tools/plot_spectra.py
@@ -28,15 +28,15 @@ print(len(single_energies[single_energies < 0]), 'singles with negative energy')
 
 seqCoin = uproot.open(str(path / 'CC_sequenceCoincidence.root') + ':sequenceCoincidence')
 print(seqCoin.num_entries, 'entries in tree sequenceCoincidence')
-df = seqCoin.arrays(['coincID', 'energy'], '(layerName=="absorber_phys")', library='pd')
+df = seqCoin.arrays(['coincID', 'eventID','energy'], '(layerName=="absorber_phys")', library='pd')
+#df = df.groupby('coincID').filter(lambda x: len(x) == 2).groupby('coincID').sum()
 print('\n'.join([f'number of coincidences with {i} singles: {len(df.groupby("coincID").size()[df.groupby("coincID").size() == i])}{" (from different events/decays)" if i > 2 else ""}' for i in range(2, df.groupby("coincID").size().max() + 1)]))
-df = df.groupby('coincID').sum()
-# print(df)
-seqCoin_energies = 1000 * df['energy']
-
+df_sum = df.drop(columns='eventID').groupby('coincID').sum()
+print(df[:20],2*'\n',df_sum[:10])
+seqCoin_energies = 1000 * df_sum['energy']
 
 fig, (ax1, ax2) = plt.subplots(2, 1, figsize=(20, 10))
-plot_args = dict(bins=emax_keV, range=(0, emax_keV), histtype='step', color='blue', linestyle='dotted')
+plot_args = dict(bins=emax_keV, range=(0, emax_keV))
 ax1.hist(np.array(single_energies), **plot_args)
 ax1.set_title('Singles / clusters')
 ax1.set_xlabel('E (keV)', loc='right')
