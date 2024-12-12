039a78bbc10d5bceeba8b7bf6b2f4612b24a8d60
tbilloud <tbilloud@gmail.com>
2024-12-12 11:27:36 +0100

    update

--- imaging/ComptonCamera_tests/tools/print_all.py
+++ imaging/ComptonCamera_tests/tools/print_all.py
@@ -1,63 +1,76 @@
 import sys
-
-import pandas
 import uproot
 from pandas import Series
-
-pandas.set_option('display.max_columns', 100)
-pandas.set_option('display.width', 400)
-pandas.set_option('display.max_rows', 1000)
+import pandas
+# pandas.set_option('display.max_columns', 100)
+# pandas.set_option('display.width', 400)
+# pandas.set_option('display.max_rows', 1000)
 pandas.set_option('display.float_format', lambda x: f'{x:.1f}')
 
 # Script to print some info about root trees produced by CCMod actor
 # ! WARNING! comment out blocks if corresponding tree was not saved
 path = '../ideal/output/'
-nentries_printed = 1000  # None to read all entries
 
 # ELECTRON ESCAPES
 tree = uproot.open(path + 'CC_eventGlobalInfo.root:EventGlobalInfo')
 print('\n =>', tree.num_entries, 'entries in tree EventGlobalInfo')
-# elec_escape = tree.arrays(library='pd', entry_stop=nentries_printed) # None to read all entries
-# print(elec_escape)
+# elec_escape = tree.arrays(library='pd', entry_stop=None) # None to read all entries
+# elec_escape = elec_escape.drop(columns=['runID'])
+# print(elec_escape.to_string(index=False))
 
 # HITS
 # stepLength[mm]: distance between two interactions of a particle (e.g.: distance between a gamma particle entering a sensitive volume and being scattered)
 # trackLength[mm]: total distance of one particle often including multiple steps. Can also be derived by the trackLocalTime.
 # processName: The process by which the particle ended its path in the sensitive detector (e.g.: Transportation (“T”), Optical Absorption(“O”), Comptonscatter(”C”), PhotoElectric(“P”), RaleighScattering(“R”)). You might be interested in distinguishing between particles that are detected at the detector(“T”) and those that were absorbed(“O”). A particle that undergoes Comptonscatter(“C”) is counted as two hits when it splits up.
+# WARNING: A particle that undergoes Comptonscatter(“C”) is counted as two hits when it splits up.
 tree = uproot.open(path + 'CC_Hits.root:Hits')
 print('\n =>', tree.num_entries, 'entries in tree Hits')
-hits = tree.arrays(library='pd', entry_stop=nentries_printed)  # None to read all entries
+hits = tree.arrays(library='pd', entry_stop=200)  # None to read all entries
 columns_to_remove = ['runID', 'time', 'sourcePDG', 'sourceEnergy', 'sourcePosX', 'sourcePosY', 'sourcePosZ', 'volumeID']
 columns_to_remove += ['posX', 'posY', 'posZ', 'localPosX', 'localPosY', 'localPosZ']
 columns_to_remove += ['nCrystalConv', 'nCrystalCompt', 'nCrystalRayl']
+columns_to_remove += ['trackLocalTime','stepLength','trackLength']
 hits = hits.drop(columns=columns_to_remove)
 hits['edep'] = round(hits['edep'] * 1000, 2)  # convert to keV
 hits['energyIniT'] = round(hits['energyIniT'] * 1000, 2)  # convert to keV
 hits['energyFinal'] = round(hits['energyFinal'] * 1000, 2)  # convert to keV
-hits['stepLength'] = round(hits['stepLength'] * 1000, 2)  # convert to um
-hits['trackLength'] = round(hits['trackLength'] * 1000, 2)  # convert to um
-hits['trackLocalTime'] = round(hits['trackLocalTime'] * 1e15, 2)  # convert to fs
-hits = hits[hits['eventID'].isin([11,33])]
-print(hits.to_string(index=False))
 # print(Series(hits['postStepProcess'].to_numpy()).value_counts(normalize=True) * 100,'\n')  # !! entry_stop = None  !!
+# print(Series(hits['layerName'].to_numpy()).value_counts(normalize=True) * 100,'\n')  # !! entry_stop = None  !!
+# print(hits.groupby('eventID')['edep'].sum())
+# hits = hits[hits['eventID'].isin([751,2027,2703])]
+#print(hits[hits['postStepProcess'] == 'compt'])
+print(hits.to_string(index=False))
 
 # SINGLES
 tree = uproot.open(path + 'CC_Singles.root:Singles')
 print('\n =>', tree.num_entries, 'entries in tree Singles')
-singles = tree.arrays(library='pd', entry_stop=nentries_printed)
-print(singles)
+singles = tree.arrays(library='pd', entry_stop=10)
+columns_to_remove = ['runID', 'time', 'sourcePDG', 'sourceEnergy', 'sourcePosX', 'sourcePosY', 'sourcePosZ', 'volumeID']
+columns_to_remove += ['localPosX', 'localPosY', 'localPosZ']
+columns_to_remove += ['nCrystalConv', 'nCrystalCompt', 'nCrystalRayl']
+singles = singles.drop(columns=columns_to_remove)
+singles['energy'] = round(singles['energy'] * 1000, 2)  # convert to keV
+singles['energyIni'] = round(singles['energyIni'] * 1000, 2)  # convert to keV
+singles['energyFinal'] = round(singles['energyFinal'] * 1000, 2)  # convert to keV
+sgroup = singles.groupby("eventID").size()
+print('\n'.join([f'number of events with {i} singles: {len(sgroup[sgroup == i])}' for i in range(1, sgroup.max() + 1)]))
+# print(singles[singles['eventID'].isin(singles.groupby('eventID').filter(lambda x: len(x) == 3)['eventID'].unique())])
+# print(singles[singles['eventID'].isin([11,33])])
+# print(Series(singles['layerName'].to_numpy()).value_counts(normalize=True) * 100,'\n')  # !! entry_stop = None  !!
+print(singles.to_string(index=False))
+# sys.exit()
 
 # COINCIDENCES
 tree = uproot.open(path + 'CC_Coincidences.root:Coincidences')
 print('\n =>', tree.num_entries, 'entries in tree Coincidences')
-coincidences = tree.arrays(library='pd', entry_stop=nentries_printed)
-print(coincidences)
+# coincidences = tree.arrays(library='pd', entry_stop=1)
+# print(coincidences)
 
 # COINCIDENCES SEQUENCES
 tree = uproot.open(path + 'CC_sequenceCoincidence.root:sequenceCoincidence')
 print('\n =>', tree.num_entries, 'entries in tree sequenceCoincidence')
-seqCoin = tree.arrays(library='pd', entry_stop=nentries_printed)
-print(len(seqCoin[seqCoin['nCrystalRayl'] > 0]), 'entries with nCrystalRayl > 0')  # !! entry_stop = None  !!
+# seqCoin = tree.arrays(library='pd', entry_stop=1)
+# print(len(seqCoin[seqCoin['nCrystalRayl'] > 0]), 'entries with nCrystalRayl > 0')  # !! entry_stop = None  !!
 # print(seqCoin)
 # print(seqCoin[seqCoin['coincID'] == 48])
 # print(seqCoin[seqCoin['nCrystalRayl'] > 0])
