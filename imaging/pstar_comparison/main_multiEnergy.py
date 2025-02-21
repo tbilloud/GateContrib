35237228566c407add1d324885927b17dfc7b613
tbilloud <tbilloud@gmail.com>
2025-02-21 18:00:09 +0100

    update test

--- imaging/pstar_comparison/main_multiEnergy.py
+++ imaging/pstar_comparison/main_multiEnergy.py
@@ -20,7 +20,7 @@ if __name__ == "__main__":
     um, mm, MeV = g4_units.um, g4_units.mm, g4_units.MeV
     sim.volume_manager.add_material_database('GateMaterials.db')
     sim.visu = False
-    thickness = 10000 * um
+    thickness = 100 * um
     sim.world.material, sim.world.size = "Vacuum", [thickness * 1.01] * 2 + [thickness * 2.01]
     sensor = sim.add_volume("Box", "sensor")
     sensor.material, sensor.size, sensor.translation = "Silicon", [thickness] * 3, [0, 0, thickness / 2]
@@ -33,7 +33,7 @@ if __name__ == "__main__":
     source.particle, source.direction.type, source.direction.momentum = "proton", "momentum", [0, 0, 1]
     sim.physics_manager.physics_list_name = 'FTFP_BERT_EMZ' # 'G4EmStandardPhysics_option4'
     sim.physics_manager.global_production_cuts.all = 1e99 * mm
-    step_size = 10 * um
+    step_size = 0.1 * um
     sensor.set_max_step_size(step_size)
     sim.physics_manager.set_user_limits_particles(['electron', 'positron', 'proton'])
 
@@ -60,7 +60,7 @@ if __name__ == "__main__":
         singles_tree.Draw(f'TotalEnergyDeposit>>+h_{energy}')
         mean = histos[idx].GetMean()
         histos[idx].GetXaxis().SetRangeUser(mean * 0.2, mean * 1.8)
-        r = histos[idx].Fit('landau', option='S', xmin=mean * 0.1, xmax=mean * 2)
+        r = histos[idx].Fit('gaus' if energy<100 else 'landau', option='S', xmin=mean * 0.1, xmax=mean * 2)
         ROOT.gPad.Update()
         histos[idx].SetDirectory(ROOT.gROOT)
         edep_MeV.append(r.Parameter(1))
@@ -75,10 +75,10 @@ if __name__ == "__main__":
     gr_pstar.GetYaxis().SetTitle('Deposited energy [keV]')
     gr_g4 = ROOT.TGraph(energies.shape[0], energies, g4_keV)
     gr_g4.SetMarkerStyle(21), gr_g4.SetMarkerColor(2)
-    legend = ROOT.TLegend(0.2, 0.7, 0.48, 0.8)
+    legend = ROOT.TLegend(0.2, 0.7, 0.35, 0.8)
     legend.AddEntry(gr_pstar, 'PSTAR', 'p')
     legend.AddEntry(gr_g4, 'Geant4', 'p')
     gr_pstar.Draw('AP'), gr_g4.Draw('SAME:P'), legend.Draw()
     gPad.SetLogx(), gPad.SetLogy(), gr_pstar.GetYaxis().SetRangeUser(10, 1e5), gPad.Update()
     print(f'geant4/pstar ratios for energies {energies} MeV:',np.round(100 * g4_keV / pstar_keV))
-    c.SaveAs(f'thickness{int(thickness / um)}um_step{int(step_size / um)}um_{sim.physics_manager.physics_list_name}.png')
+    c.SaveAs(f'thickness{int(thickness / um)}um_step{step_size / um}um_{sim.physics_manager.physics_list_name}.png')
