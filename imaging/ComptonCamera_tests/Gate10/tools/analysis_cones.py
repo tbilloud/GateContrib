0be3150c93f97e6f9294f97d0df60704f92e0865
tbilloud <tbilloud@gmail.com>
2025-04-22 14:32:34 +0200

    cleanup code

--- imaging/ComptonCamera_tests/Gate10/tools/analysis_cones.py
+++ imaging/ComptonCamera_tests/Gate10/tools/analysis_cones.py
@@ -4,17 +4,10 @@
 
 import os
 import sys
-import numpy as np
 import pandas
 import uproot
-import SimpleITK as sitk
-import matplotlib.pyplot as plt
-import pandas as pd
-from pandas import Series
 from analysis_pixelClusters import X_um, Y_um, EVENTID, ENERGY_keV
-import tools.analysis_basics as analysis_basics
 from tools.utils import *
-from tools.utils import print_hits_inG4format
 from opengate.logger import global_log
 
 pandas.set_option('display.max_columns', 100)
@@ -114,26 +107,26 @@ def pixelClusters2cones_byEvtID(pixelClusters, source_MeV, thickness_um):
         # TODO: 1) Distinguish compton vs photo-electric interactions
         group = group.sort_values(ENERGY_keV)
         # print(group)
-        photoelec_interaction = group.iloc[0]
-        compton_interaction = group.iloc[1]
+        clust_photoel = group.iloc[0]
+        clust_compton = group.iloc[1]
 
         # TODO: 2) Calculate depth difference
         # delta_z = ... charge_carrier_speed * (TOA_photoelec - TOA_compton)
 
         # TODO: 3) Calculate absolute depth of Compton interaction (apex)
-        apex_z = thickness_um / 2
+        z_compton_um = thickness_um / 2
         # OR
         # use cluster size (and energy?)
 
         # TODO: 4) Complete 3D positions
-        pos_compton = [compton_interaction[X_um], compton_interaction[Y_um], 0]
-        pos_photoelec = [photoelec_interaction[X_um], photoelec_interaction[Y_um], thickness_um]
+        pos_compton = [clust_compton[X_um], clust_compton[Y_um], z_compton_um]
+        pos_photoelec = [clust_photoel[X_um], clust_photoel[Y_um], thickness_um]
 
         # TODO: 5) Construct cone
         apex = pos_compton
         direction = np.array(apex) - np.array(pos_photoelec)
         direction = (direction / np.linalg.norm(direction)).tolist()
-        E1_MeV = photoelec_interaction[ENERGY_keV] / 1000
+        E1_MeV = clust_photoel[ENERGY_keV] / 1000
         cosT = 1 - (0.511 * E1_MeV) / (source_MeV * (source_MeV - E1_MeV))
         apex_mm = [apex[0] / 1000, apex[1] / 1000, apex[2] / 1000]
         cones.append([eventid] + apex_mm + direction + [cosT] + [200])
